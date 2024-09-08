223f143d802cd89cc42d0debdfd8fdc07dc67d58
Nbiba Bedis <bedisnbiba@gmail.com>
2024-09-08 22:43:09 +0100

    webview

---
+++ webview/main.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S deno run --allow-all
+import { Webview } from "jsr:@webview/webview";
+
+const worker = new Worker(import.meta.resolve("../backend/server.ts"), {
+  type: "module",
+});
+
+const webview = new Webview();
+webview.title = "Ping";
+webview.navigate("http://localhost:3000");
+webview.run();
+
+Deno.exit(0);
